0649c2a05e0c1c83f5bbecdf9896a0e8121c34c9
MILLENA FERREIRA DE SOUSA <22282227@dsn127m1242374.cfp127aluno.local>
2023-09-11 15:40:21 -0300

    Componentização : Botão Padrão Finalizado

---
+++ app/src/main/java/br/senai/sp/jandira/ayancare_frontmobile/components/DefaultButton.kt
@@ -0,0 +1,53 @@
+package br.senai.sp.jandira.ayancare_frontmobile.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+
+fun DefaultButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .width(280.dp)
+            .height(45.dp)
+            .shadow(
+                elevation = 4.dp,
+                spotColor = Color(0x40000000),
+                ambientColor = Color(0x40000000)
+            )
+            .background(color = Color(0xFF35225F)),
+        Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            fontWeight = FontWeight(600),
+            color = Color(0xFFFFFEFE),
+        )
+    }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultButtonPreview() {
+    DefaultButton(
+        text = "Entrar",
+        onClick = {}
+    )
+}
